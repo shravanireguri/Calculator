935db479a82b5bb5d3e97d0f452e7f2afff76761
Shravani Reguri <shravanireguri99@gmail.com>
2024-03-20 15:08:08 +0530

    Subtraction of two numbers

---
+++ src/main/java/Subtraction.java
@@ -0,0 +1,12 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Subtraction {
+	public int subtract() {
+		int num1 = 20;
+		int num2 = 10;
+		
+		int res = num1 - num2;
+		
+		return res;
+	}
+}
--- src/main/java/com/qualizeal/javaapp/App.java
+++ src/main/java/com/qualizeal/javaapp/App.java
@@ -1,11 +1,16 @@
 package com.qualizeal.javaapp;
 
 import com.qualizeal.javaapp.mathematics.Addition;
+import com.qualizeal.javaapp.mathematics.Subtraction;
 
 public class App {
     public static void main(String[] args ) {
         Addition addition = new Addition();
-		int ans = addition.add();
-		System.out.println(ans);
+		int add_ans = addition.add();
+		System.out.println(add_ans);
+		
+		Subtraction subtraction = new Subtraction();
+		int sub_ans = subtraction.subtract();
+		System.out.println(sub_ans);
     }
 }
